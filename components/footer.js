d5f5e7a33374fa42017fd28022116e019a1b7fda
Rochana <rochana.odu@gmail.com>
2024-09-29 01:10:45 -0400

    seo added

--- components/footer.js
+++ components/footer.js
@@ -10,19 +10,19 @@ class Footer extends HTMLElement {
         <div class="py-3 px-md-5 container-fluid">
             <div class="row">
                 <div class="col-auto col-md-5 align-middle">
-                    <p class="mb-0" style="color: var(--light-gray);">© 2024 Global Student Friendship</p>
+                    <p class="mb-0" style="color: var(--monarch-blue-80w);">© 2024 Global Student Friendship</p>
                     <p class="my-0 pt-0"><a class="" href="mailto:gsfodu@gmail.com">gsfodu@gmail.com</a></p>
 
                 </div>
 
 
                 <div class="col col-md-7 text-right">
-                    <p class="mb-0" style="color: var(--light-gray);">Quick Links</p>
+                    <p class="mb-0" style="color: var(--monarch-blue-80w);">Quick Links</p>
                     <p class="my-0 pt-0">
                         <a href=""><i class="mr-3 footer-icons fa-solid fa-house" aria-hidden="true"></i></a>
-                        <a href="" target="_blank"><i class="mr-3 fab footer-icons fa-facebook" aria-hidden="true"></i></a>
+                        <a href="https://www.facebook.com/globalstudentfriendship" target="_blank"><i class="mr-3 fab footer-icons fa-facebook" aria-hidden="true"></i></a>
                         <a href="/pages/events.html" target=""><i class="mr-3 footer-icons fa-solid fa-calendar-days" aria-hidden="true"></i></a>
-                        <a href="" target="_blank"><i class="footer-icons fa-solid fa-address-book" aria-hidden="true"></i></a>
+                        <a href="/pages/contact_us.html" target="_blank"><i class="footer-icons fa-solid fa-address-book" aria-hidden="true"></i></a>
                     </p>
                 </div>
             </div>
