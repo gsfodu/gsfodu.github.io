1be9f4c4d7f3712fba972e30982e1e202818d6ec
Rochana <rochana.odu@gmail.com>
2024-11-14 01:13:38 -0500

    ministries updated

--- components/footer-lemos.js
+++ components/footer-lemos.js
@@ -10,7 +10,7 @@ class Footer_L extends HTMLElement {
         <div class="pt-1 pb-2 px-md-5 container-fluid">
             <div class="row">
                 <div class="col-auto col-md-5 align-middle">
-                    <p class="mb-0 small" style="color: #DBE2EF;">Developed by <a style="color: #0077b6;" href="https://rochanaro.github.io/" target="_blank">RochanaRO</a> via <a style="color: #0077b6;" href="https://github.com/" target="_blank">GitHub<i class="mx-1 fab footer-icons fa-github" aria-hidden="true"></i></a> ©2024</p> 
+                    <p class="mb-0 small" style="color: #DBE2EF;">Developed by <a style="color: #0077b6;" href="https://rochanaro.github.io/" target="_blank">RochanaRO</a> hosted via <a style="color: #0077b6;" href="https://github.com/" target="_blank">GitHub<i class="mx-1 fab footer-icons fa-github" aria-hidden="true"></i></a> ©2024</p> 
 
                 </div>
             </div>
