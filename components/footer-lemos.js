1ba1d3f01e1d3a1ba063528c20b54b47019f680a
Rochana <rochana.odu@gmail.com>
2026-08-27 05:13:42 -0400

    footer updated - suhas added

--- components/footer-lemos.js
+++ components/footer-lemos.js
@@ -9,9 +9,14 @@ class Footer_L extends HTMLElement {
 
         <div class="pt-1 pb-2 px-md-5 container-fluid">
             <div class="row">
-                <div class="col-auto col-md-5 align-middle">
-                    <p class="mb-0 small" style="color: #DBE2EF;">Developed by <a style="color: #0077b6;" href="https://suhaspanuganti.github.io/" target="_blank">Suhas Panuganti</a> hosted via <a style="color: #0077b6;" href="https://github.com/" target="_blank">GitHub<i class="mx-1 fab footer-icons fa-github" aria-hidden="true"></i></a> ©2026</p> 
-
+                <div class="col-auto align-middle">
+                    <p class="mb-0 small" style="color: #DBE2EF;">
+                        Maintained by <a style="color: #0077b6;" href="https://suhaspanuganti.github.io/" target="_blank">Suhas Panuganti</a>
+                        &middot;
+                        Developed by <a style="color: #0077b6;" href="https://rochanaro.github.io/" target="_blank">rochanaro<i class="mx-1 fab footer-icons fa-github" aria-hidden="true"></i></a>
+                        &middot;
+                        ©2026
+                    </p>
                 </div>
             </div>
         </div>
